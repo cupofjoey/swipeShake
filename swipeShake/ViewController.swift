d27df550924e59e56d25639c7e9f66069de7748e
Joe Hill <joe@jkh3.com>
2017-05-01 21:58:50 -0400

    Practice with UIGestureRecognizer and UIEventSubtype Shaken

---
+++ swipeShake/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  swipeShake
+//
+//  Created by BishopHill on 5/1/17.
+//  Copyright © 2017 Dumb Unicorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture:)))
+            
+            swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture: )))
+        
+            swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        
+        
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        if event?.subtype == UIEventSubtype.motionShake {
+            
+            print("Device was shaken")
+            
+        }
+        
+    }
+    
+    func swiped(gesture: UIGestureRecognizer) {
+        
+        if let swipedGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipedGesture.direction {
+                
+            case UISwipeGestureRecognizerDirection.right:
+                print("User Swiped Right")
+            case UISwipeGestureRecognizerDirection.left:
+                print("User Swiped Left")
+            default:
+                break
+                
+            }
+            
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
